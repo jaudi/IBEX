993d142c705b33a52db953999c894536b8765628
jaudi <javier.audibert@gmail.com>
2024-10-21 20:07:16 +0100

    Update IBEX_components.py

--- IBEX_components.py
+++ IBEX_components.py
@@ -4,7 +4,7 @@ def app():
     
     st.title("Companies listed on the IBEX 35")
             
-    Companies=pd.read_csv("https://github.com/jaudi/IBEX/blob/main/ibex-components.csv")
+    Companies=pd.read_csv("https://raw.githubusercontent.com/jaudi/IBEX/main/ibex-components.csv")
     Companies=Companies.set_index("Ticker")
 
     st.dataframe(Companies)
